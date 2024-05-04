c05ac506dcde04bef919b8437207bcc8f8bcbb93
Ryan Aragão <ryanaragao012@gmail.com>
2024-05-04 16:09:56 -0300

    desabilita o botao de submit enquanto realiza o cadastro

--- codigo-fonte/cadastro/script.js
+++ codigo-fonte/cadastro/script.js
@@ -1,6 +1,7 @@
 const usuario = document.getElementById('usuario');
 const senha = document.getElementById('senha');
 const confirmeSenha = document.getElementById('confirmeSenha');
+const btnCadastro = document.getElementById('btnCadastro')
 const form = document.getElementById('form-cadastro');
 
 form.addEventListener('submit', e => {
@@ -10,36 +11,41 @@ form.addEventListener('submit', e => {
 });
 
 function checkInputs(){
+    btnCadastro.disabled = true
     const usuarioValue = usuario.value.trim();
     const senhaValue = senha.value.trim();
     const confirmeSenhaValue = confirmeSenha.value.trim();
     
     if(usuario.value === '' || usuario.value == null)
     {
+        btnCadastro.disabled = false
        return alert('Preencha o nome de Úsuario');
         
     } 
     
     if(senha.value ===''){
+        btnCadastro.disabled = false
         return  alert('Preencha a senha');
     } 
     
     if(senha.value.lenght <= 5)
     {
-        
+        btnCadastro.disabled = false
       return  alert('Senha deve conter mais de 6 caracteres');
     
     }
     
     if(senha.value.lenght >= 20)
     {
-       
+        btnCadastro.disabled = false
         return alert('Senha não pode conter mais de 20 caracteres'); 
     } 
 
     if(confirmeSenhaValue === ''){
+        btnCadastro.disabled = false
       return  alert( 'Preencha a Confirmação de Senha');
     } else if(senhaValue !== confirmeSenhaValue){
+        btnCadastro.disabled = false
        return alert( 'As senhas não conferem');
     } 
 
@@ -53,6 +59,7 @@ function cadastrar () {
     let listaUser =JSON.parse(localStorage.getItem('listaUser') || '[]')
     
     if(listaUser.find(user=> user.userCad === usuario.value)){
+        btnCadastro.disabled = false
         return alert('Este usuário já existe!')
     }
 
@@ -64,6 +71,6 @@ function cadastrar () {
     )
     localStorage.setItem('listaUser', JSON.stringify(listaUser))
 
-    // Limpa os campos
+    
     window.location.href = '/codigo-fonte/login'
 }
