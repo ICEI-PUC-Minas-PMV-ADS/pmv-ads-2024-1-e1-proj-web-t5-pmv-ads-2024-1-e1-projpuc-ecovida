c35abdef9b8c6502356f22f4a19cbaeac32e5ecc
Ryan Aragão <ryanaragao012@gmail.com>
2024-06-09 15:44:59 -0300

    adiciona funcionalidade login

---
+++ codigo-fonte/login/script.js
@@ -0,0 +1,30 @@
+const usuario = document.getElementById('NM_User');
+const senha = document.getElementById('TX_Password');
+const form = document.getElementById('form-login');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs(){
+    const usuarioValue = usuario.value.trim();
+    const senhaValue = senha.value.trim();
+    
+    let listaUser =JSON.parse(localStorage.getItem('listaUser') || '[]')
+
+    let exist = listaUser.find(user=> user.NM_User === usuarioValue)
+
+    if(exist){
+      if(exist.TX_Password === senhaValue){
+         window.location.href = '/codigo-fonte/home'
+      }else{
+        return  alert('Usuário ou senha incorretos!')
+      }
+    }else{
+     return alert('Usuário ou senha incorretos!')
+    }
+
+
+}
